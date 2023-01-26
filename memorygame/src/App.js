f96609cedbd7b97160acf1fbd1f59a4b472eda49
Owen Lenihan <owenlenihan101@gmail.com>
2023-01-26 21:40:49 +0000

    Removed unneeded react files and set up basic game logic

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [colour, setColour] = useState('black');
+
+  const changeColourClick = () => {
+    if (colour === 'black') {
+      setColour('red');
+    } else {
+      setColour('black')
+    }
+  };
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {    
+    document.title = `You clicked ${count} times`;  
+  });
+
+  useEffect(() => {
+    console.log("Hi")
+  }, [colour]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        id="myDiv1"
+        onClick={changeColourClick}
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          left: "50%",
+          top: "50%",
+          backgroundColor: colour,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
+      <p>You clicked {count} times</p>
+      <div
+        id="myDiv2"
+        onClick={() => setCount(count + 1)}
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          left: "50%",
+          top: "50%",
+          backgroundColor: colour,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
     </div>
   );
-}
+};
 
 export default App;
